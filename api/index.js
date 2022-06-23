f01675bffd300bc688f92ed86a17690b43e5fe93
adnanali000 <adnan.museel123@gmail.com>
2022-06-24 03:03:11 +0500

    hotel routes model and controller done

--- api/index.js
+++ api/index.js
@@ -34,6 +34,19 @@ app.use("/api/users",usersRoute);
 app.use("/api/rooms",roomsRoute);
 app.use("/api/hotels",hotelsRoute);
 
+//error handling middleware
+app.use((err,req,res,next)=>{
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong";
+
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    })
+})
+
 
 
 
